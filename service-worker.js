e3f466d41ce2f4f599348b0ec420f24158487b01
ElyasMTarek <m.tarek@gmx.at>
2019-01-24 09:28:28 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ElyasMTarek/react-typescript-sentry/precache-manifest.8471f331397890380e02f6e23d8ad2b2.js"
+  "/ElyasMTarek/react-typescript-sentry/precache-manifest.16e18ac6c161ca878c4aa45813033214.js"
 );
 
 workbox.clientsClaim();
